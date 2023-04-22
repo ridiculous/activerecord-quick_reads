a1e0da4f1cf18c4cc0090e4fe8b70f360a2d3eae
Ryan Buckley <ryan.buckley@envoy.com>
2023-04-22 12:15:29 -0700

    Rename internal model and instance to limit conflicts

--- lib/activerecord/quick_read/lite_base.rb
+++ lib/activerecord/quick_read/lite_base.rb
@@ -2,31 +2,33 @@ module ActiveRecord
   module QuickRead
     module LiteBase
       def method_missing(name, *args, &block)
-        return super unless subject.respond_to?(name)
+        return super unless _ar_instance.respond_to?(name)
 
-        subject.send(name, *args, &block)
+        _ar_instance.send(name, *args, &block)
       end
 
       def respond_to_missing?(*args)
-        subject.respond_to?(*args) || super
-      end
-
-      def subject
-        @subject ||= model.from_hash(to_h)
-      end
-
-      # Since the struct that includes this module is defined within the model's namespace
-      def model
-        self.class.module_parent
+        _ar_instance.respond_to?(*args) || super
       end
 
       def reload(*)
-        source = @subject&.reload
-        source ||= model.unscoped.where(id: id).select(members.join(', ')).quick_read
+        source = @_ar_instance&.reload
+        source ||= _ar_model.unscoped.where(id: id).select(members.join(', ')).quick_read
         return false unless source
 
         members.each { |attr| send(:"#{attr}=", source.send(attr)) }
         self
+      end
+
+      # Private
+
+      def _ar_instance
+        @_ar_instance ||= _ar_model.from_hash(to_h)
+      end
+
+      # Since the struct that includes this module is defined within the model's namespace
+      def _ar_model
+        self.class.module_parent
       end
     end
   end
